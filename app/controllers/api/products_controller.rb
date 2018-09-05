3ab1fb7633ccc115fa440786286ca970353d06f0
Adam <ahanson845@gmail.com>
2018-09-04 21:50:44 -0500

    updated mini capstone

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -16,8 +16,11 @@ class Api::ProductsController < ApplicationController
                            image_url: params[:image_url],
                            description: params[:description]
                            )
-    @product.save
-    render 'show.json.jbuilder'
+    if @product.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
   end
   
   def update
@@ -29,7 +32,11 @@ class Api::ProductsController < ApplicationController
     @product.description = params[:description] || @product.description
     @product.save
 
-    render 'show.json.jbuilder'
+    if product.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
