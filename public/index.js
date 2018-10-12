761858f3525c074e467830a3824d88d05c76cbd0
Adam <ahanson845@gmail.com>
2018-10-11 21:33:59 -0500

    updated front end

---
+++ public/index.js
@@ -0,0 +1,24 @@
+var productTemplate = document.querySelector('#product-card');
+var productContainer = document.querySelector('.row');
+
+axios.get("http://localhost:3000/api/products").then(function(response) {
+var products = response.data;
+console.log(products);
+
+products.forEach(function(product) {
+  var productClone = productTemplate.content.cloneNode(true);
+
+  productClone.querySelector('.product-title').innerText = product.name;
+  productClone.querySelector('.product-price').innerText = product["price"];
+  productClone.querySelector('.product-description').innerText = product.description;
+ 
+
+  productContainer.appendChild(productClone);
+
+});
+
+
+});
+
+
+
